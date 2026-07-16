e0a723c21fcf96327424c8eca2880d152b988d39
phdeandrade <phenrique.and13@gmail.com>
2026-07-16 11:04:56 -0300

    feat: config do navbar

--- frontend/b-tree-visualizer-frontend/src/components/NavBar/NavBar.tsx
+++ frontend/b-tree-visualizer-frontend/src/components/NavBar/NavBar.tsx
@@ -5,7 +5,13 @@ function NavBar() {
     return (
         <nav className="navbar">
             <img src={graphIcon} alt="Ícone da Árvore" className="nav-icon"/>
-            B-Tree Visualizer
+
+            <span>B-Tree Visualizer</span>
+
+            <ul className="ul-navbar">
+                <li><a href="/">About</a></li>
+                <li><a href="/">Home</a></li>
+            </ul>
         </nav>
     )
 }
